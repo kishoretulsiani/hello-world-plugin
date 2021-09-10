0b8738f5796eb4fc0a6914a38d3a985db73d88a1
GitHub Action <action@github.com>
2021-09-10 18:01:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kishoretulsiani/helloworldplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kishoretulsiani.helloworldplugin.services
+
+import com.github.kishoretulsiani.helloworldplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kishoretulsiani/helloworldplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kishoretulsiani/helloworldplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kishoretulsiani.helloworldplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kishoretulsiani.helloworldplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
